e37a073b855559450431b813d3c0585d3f87a8bc
Surafel <surafelshif@gmail.com>
2024-10-27 17:19:08 +0200

    rahtal to null if removed

--- app/Http/Resources/RahtalResource.php
+++ app/Http/Resources/RahtalResource.php
@@ -10,11 +10,16 @@ class RahtalResource extends JsonResource
 {
     public function toArray($request)
     {
+        $imageUrl = $this->image ? config('app.url') . ":8000" . Storage::url($this->image->image_path) : null;
+
+        if ($imageUrl && str_contains($this->image->image_path, 'placeholder')) {
+            $imageUrl = null;
+        }
+
         return [
             'uuid' => $this->uuid,
             'full_name' => $this->full_name,
-            'image_url' => $this->image ? config('app.url') . ":8000" . Storage::url($this->image->image_path) : null,
-
+            'image_url' => $imageUrl,
         ];
     }
 }

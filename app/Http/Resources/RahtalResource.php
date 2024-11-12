c78b009b9458b2bb660d8be1f369b7b775462022
Surafel <surafelshif@gmail.com>
2024-11-12 19:34:31 +0200

    change image_url property to image

--- app/Http/Resources/RahtalResource.php
+++ app/Http/Resources/RahtalResource.php
@@ -12,7 +12,7 @@ class RahtalResource extends JsonResource
             'uuid' => $this->uuid,
             'full_name' => $this->full_name,
             'personal_number' => $this->personal_number,
-            'image_url' => $this->image->image_path ? config('filesystems.storage_path') . $this->image->image_path : null,
+            'image' => $this->image->image_path ? config('filesystems.storage_path') . $this->image->image_path : null,
         ];
     }
 }
